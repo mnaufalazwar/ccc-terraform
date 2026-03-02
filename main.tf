f94296c994f9bf58fc6ce25c4f5a0ea2ce799731
falazwar <mnaufalazwar@gmail.com>
2026-03-02 15:53:31 +1100

    add remote repository

--- main.tf
+++ main.tf
@@ -7,6 +7,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "ccc-tfstate-696637902012-ap-southeast-1"
+    key            = "ccc/aws/prod/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "ccc-terraform-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
